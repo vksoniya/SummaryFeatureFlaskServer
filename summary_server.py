5b1c9c65b083f3763e404d8c5ed9ea3b53c37541
8vijayak <nAract5r2i>
2020-10-13 03:26:55 +0530

    Adding dynamic Values to index.html

--- summary_server.py
+++ summary_server.py
@@ -1,11 +1,17 @@
 from __future__ import unicode_literals
 from flask import Flask, render_template, url_for, request
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return render_template('index.html')
+        conf_id = "12345"
+        meeting_start_time = time.time()
+        participant_1 = "Soniya"
+        participant_2 = "Vindhya"
+        final_summary = "Some random stuff that we talk about"
+        return render_template('index.html',conf_id=conf_id,meeting_start_time=meeting_start_time,participant_1=participant_1,participant_2=participant_2,final_summary=final_summary)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=7030, debug=True)
