1d658b53b90a020b394ff2122d39f7e3b6d17650
8vijayak <nAract5r2i>
2020-10-13 02:29:31 +0530

    simple flask server

---
+++ summary_server.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+        return "Hello World"
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=7030, debug=True)
