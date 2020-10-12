0cac16ec25f1b506aa82525d94158203bb0c16fb
8vijayak <nAract5r2i>
2020-10-13 02:40:36 +0530

    modified simple server

--- summary_server.py
+++ summary_server.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
         return render_template('index.html')
 
 if __name__ == '__main__':
