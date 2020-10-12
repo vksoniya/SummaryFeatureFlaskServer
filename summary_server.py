772a46b451b5612ba3556679603aced561bef422
8vijayak <nAract5r2i>
2020-10-13 03:34:05 +0530

    Added getting meeting information

--- summary_server.py
+++ summary_server.py
@@ -1,14 +1,19 @@
 from __future__ import unicode_literals
 from flask import Flask, render_template, url_for, request
 import time
+from utils.getMeetingInfo import getcurrentMeetingInfo
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-        conf_id = "12345"
-        meeting_start_time = time.time()
-        participant_1 = "Soniya"
+        
+        #Step 1: Get the Current Meeting Information
+        currentMeetingInfo = getcurrentMeetingInfo()
+        conf_id = currentMeetingInfo['voiceConfID']
+        meeting_start_time = currentMeetingInfo['recordTimeStamp']
+        participantList = currentMeetingInfo['userNames']
+        participant_1 = participantList
         participant_2 = "Vindhya"
         final_summary = "Some random stuff that we talk about"
         return render_template('index.html',conf_id=conf_id,meeting_start_time=meeting_start_time,participant_1=participant_1,participant_2=participant_2,final_summary=final_summary)
