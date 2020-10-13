7e22e5b61916126e5538052a119749aa2adde29f
8vijayak <nAract5r2i>
2020-10-13 15:39:11 +0530

    added refresh option

--- summary_server.py
+++ summary_server.py
@@ -5,23 +5,37 @@ from utils.getMeetingInfo import getcurrentMeetingInfo
 
 app = Flask(__name__)
 
+CONF_ID = ""
+MEETING_START_TIME = ""
+PARTICIPANT_LIST = ""
+FINAL_SUMMARY = ""
+
 @app.route('/')
 def index():
         pStr = ""
         #Step 1: Get the Current Meeting Information
         currentMeetingInfo = getcurrentMeetingInfo()
-        conf_id = currentMeetingInfo['voiceConfID']
-        meeting_start_time = currentMeetingInfo['recordTimeStamp']
+        CONF_ID = currentMeetingInfo['voiceConfID']
+        MEETING_START_TIME = currentMeetingInfo['recordTimeStamp']
         participantList = currentMeetingInfo['userNames']
         for p in participantList:
             pStr = pStr + str(p) + ", "
-        participant_list = pStr
+        PARTICIPANT_LIST = pStr
         #Step 2: Load <confid>_summary.txt content
         #currentSummaryFile = "MeetingSummaryData/" + conf_id + "_summary.txt" # Actual file
         currentSummaryFile = "MeetingSummaryData/88503_summary.txt" #This filename will
         fSummary = open(currentSummaryFile, "r")
         final_summary = fSummary.read()
-        return render_template('index.html',conf_id=conf_id,meeting_start_time=meeting_start_time,participant_list=participant_list,final_summary=final_summary)
+        FINAL_SUMMARY = final_summary
+        return render_template('index.html',conf_id=CONF_ID,meeting_start_time=MEETING_START_TIME,participant_list=PARTICIPANT_LIST,final_summary=FINAL_SUMMARY)
+
+@app.route('/refresh',methods=['GET','POST'])
+def refresh():
+    final_summary = ""
+    if request.method == 'POST':
+        final_summary = "underconstruction"
+        FINAL_SUMMARY = final_summary
+    return render_template('index.html',conf_id=CONF_ID,meeting_start_time=MEETING_START_TIME,participant_list=PARTICIPANT_LIST,final_summary=FINAL_SUMMARY)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=7030, debug=True)
